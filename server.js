8796d15e42c3cd3bf15011618d1d3695ed1ef56c
Aariv <arivbits@gmail.com>
2023-03-18 19:23:24 +0530

    Added comments

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json()); // Convert all the request & response as JSON
 app.use('/api/contacts', require("./routes/contactRoutes"));
 app.use('/api/users', require("./routes/userRoutes"));
-app.use(errorHandler);
+
+app.use(errorHandler); // Error Handler
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
