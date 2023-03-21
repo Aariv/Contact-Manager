08a27d8b702522773a72cb43c96e0b4702670d9d
Aariv <arivbits@gmail.com>
2023-03-21 08:09:24 +0530

    Worked on User Registration

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -38,6 +38,16 @@ const createUser = asyncHandler(async (req, res) => {
     // Hash Password
     const hashedPassword = await bcrypt.hash(password, 10);
     console.log("hashed password", hashedPassword);
+    const user = await User.create({
+        username, email, password: hashedPassword
+    });
+    console.log(`User Created ${user}`);
+    if(user) {
+        res.status(201).json({_id: user.id, email: user.email});
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
     res
     .status(200)
     .json({message: `Create a user ${req.body}`});
